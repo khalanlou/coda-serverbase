be2ad2ae24efce3f820665fad6033fa0c9a3704d
centos <centos@hicoda.ir>
2020-09-27 13:10:20 +0330

    s

--- coda/urls.py
+++ coda/urls.py
@@ -19,12 +19,14 @@ from django.conf.urls import url , include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^' , include('mysite.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-
 ]
+
+
 if settings.DEBUG:
     urlpatterns += static( settings.MEDIA_URL ,document_root = settings.MEDIA_ROOT )
     urlpatterns += static( "/contact_us/static/" ,document_root = settings.STATIC_ROOT)
